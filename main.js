0d67ee40bdfe071b95eaebdc5877073aa8bf9f3a
juh290 <jullia.gabriel@escola.pr.gov.br>
2026-06-03 12:17:03 -0300

    aula 03/06

--- main.js
+++ main.js
@@ -6,9 +6,14 @@ for(let i =0; i < botoes.length; j++){
 
         for (let j = 0; j < botoes.length; j++) {
             botoes[j].classList.remove("ativo");
+            textos[j].classList.remove("ativo");
         }
 
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
 }
+
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date("2026-10-10T00-00-00")
+let tempoAtual = new Date();
