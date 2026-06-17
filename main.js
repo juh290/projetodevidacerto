dd66625d2fd3c9db4f35cd7656c0f8593870a138
juh290 <jullia.gabriel@escola.pr.gov.br>
2026-06-17 12:09:00 -0300

    Aula final

--- main.js
+++ main.js
@@ -16,9 +16,9 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2026-11-25T00:00:00");
-const tempoObjetivo2 = new Date("2027-12-31T00:00:00");
+const tempoObjetivo2 = new Date("2027-04-30T00:00:00");
 const tempoObjetivo3 = new Date("2027-03-01T00:00:00");
-const tempoObjetivo4 = new Date("2027-02-30T00:00:00");
+const tempoObjetivo4 = new Date("2027-02-28T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
 
